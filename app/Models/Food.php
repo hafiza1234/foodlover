333ccd277a97b60be8b1f3178a7d9cbd93b6b5e4
Bokul <bokulhafiza@gmail.com>
2021-01-20 23:22:37 +0600

    add property in food model

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Food extends Model
 {
     use HasFactory;
+    protected $table = 'foods';
+
+    protected $fillable = ['name', 'description'];
+    
 }
