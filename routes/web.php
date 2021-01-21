e819fb56f528e692f81cf5f3c8c18e89ffab1f4f
Md Mahbub Rabbani <mahbub.rucse@gmail.com>
2021-01-21 19:43:19 +0600

    add MenuController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\MenuController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,3 +39,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
+    Route::get('menus', [MenuController::class, 'index']);
+});
