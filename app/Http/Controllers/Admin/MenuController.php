8dd8d1533609a0b1a81f7b426bb08201b53407df
Md Mahbub Rabbani <mahbub.rucse@gmail.com>
2021-01-21 20:54:24 +0600

    add menus admin view

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Menu;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
@@ -10,5 +11,42 @@ class MenuController extends Controller
     public function index()
     {
         return 'Bokul';
+    }
+
+    public function create()
+    {
+        return view('admin.menu.create');
+    }
+
+    public function store(Request $request)
+    {
+        $menu = Menu::create($request->all());
+
+        return redirect('admin/menus');
+    }
+
+    public function edit($id)
+    {
+        $menu = Menu::find($id);
+
+        return view('admin.menu.edit');
+    }
+
+    public function update($id, Request $request)
+    {
+        $menu = Menu::find($id);
+
+        $menu->update($request->all());
+
+        return redirect('admin/menus');
+    }
+
+    public function destroy($id)
+    {
+        $menu = Menu::find($id);
+
+        $menu->delete();
+        
+        return redirect('admin/menus');
     }
 }
