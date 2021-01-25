c8e6b9194a77a8e2129b926c7b65d08eac46faff
Bokul <bokulhafiza@gmail.com>
2021-01-25 11:18:57 +0600

    fix css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
         <link rel="stylesheet" href="/css/google-font.css">
         <link type="text/css" rel="stylesheet" href="/css/font-awesome.min.css">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/bootstrap.min.css"  crossorigin="anonymous">
         {{-- <link type="text/css" rel="stylesheet" href="css/media.css"> --}}
         
     </head>
@@ -42,8 +42,8 @@
             </main>
         </div>
 
-        <script src="/js/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="/js/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+        <script src="/js/jquery-3.5.1.slim.min.js"  crossorigin="anonymous"></script>
+        <script src="/js/popper.min.js" crossorigin="anonymous"></script>
+        <script src="/js/bootstrap.min.js"  crossorigin="anonymous"></script>
     </body>
 </html>
